57ac601c1c79ef1b9f9a5ff6c01f2e11613cb55e
afd <afd@alup5.academiapostal.es.local>
2020-01-24 20:10:36 +0100

    nuevaver

--- index.js
+++ index.js
@@ -7,9 +7,26 @@
  */
 export function calcularTotal(carrito) {
  var total=0;
+
  for (var i=0; i<carrito.length; i++){
-   total+=carrito[i].precio;   
+  if (carrito[i].cantidad){
+    total+=carrito[i].precio * carrito[i].cantidad;
+  } else{
+    total +=carrito[i].precio;
+  }  
  }
  return total;
+}
+
+export function calcularpeso(carrito){
+  var total=0;
+  for (var i=0; i<carrito.length; i++){
+    if (carrito[i].cantidad){
+      total+=carrito[i].peso * carrito[i].cantidad;
+    } else{
+      total+=carrito[i].peso;
+    }
+  }
+  return total;
 }
 
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-import { calcularTotal} from './index';
+import { calcularTotal, calcularpeso} from './index';
 import { equal } from "assert";
 
 describe(('Cálculo del total'), () => {
@@ -10,4 +10,29 @@ describe(('Cálculo del total'), () => {
     equal(calcularTotal([{producto: "pan", precio: 1},
     {producto:"leche", precio:1}]),2)
   })
+  it ('comprar huevos',()=>{
+    equal(calcularTotal([
+    {producto:"huevo", precio:30, cantidad:12}]), 360)
+  })
 })
+
+describe(('calculo del peso'),()=>{
+ it('carrito vacío',()=>{equal(calcularpeso([]),0)}); 
+ it ('comprar pan',()=>{
+  equal(calcularpeso([{producto: "pan", peso: 80, cantidad: 2}]),160)
+}) 
+it ('comprar pan y leche',()=>{
+  equal(calcularpeso([
+  {producto:"huevo", peso:180, cantidad:12}]), 2160)
+})
+it ('comprar huevos',()=>{
+  equal(calcularpeso([
+  {producto:"huevo", peso:150, cantidad:12}]), 1800)
+})
+it ('pan',()=>{
+  equal(calcularpeso([
+  {producto:"pan", peso:80 }]), 80)
+})
+
+});
+
